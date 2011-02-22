05f13b4eab08dd7239b1b46a307fba5f803f411a
Dylan Price <the.dylan.price@gmail.com>
2011-02-21 16:45:00 -0800

    Fixed GoogleSpreadsheet trouble with special characters in headers and added parent-uid to repeat spreadsheets.

---
+++ src/org/opendatakit/aggregate/form/SubmissionAssociationTable.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright (C) 2010 University of Washington
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.opendatakit.aggregate.form;
+
+import org.opendatakit.aggregate.CallingContext;
+import org.opendatakit.common.persistence.CommonFieldsBase;
+import org.opendatakit.common.persistence.DataField;
+import org.opendatakit.common.persistence.Datastore;
+import org.opendatakit.common.persistence.PersistConsts;
+import org.opendatakit.common.persistence.DataField.IndexType;
+import org.opendatakit.common.persistence.exception.ODKDatastoreException;
+import org.opendatakit.common.security.User;
+
+/**
+ * 
+ * @author wbrunette@gmail.com
+ * @author mitchellsundt@gmail.com
+ * 
+ */
+public class SubmissionAssociationTable extends CommonFieldsBase {
+	private static final String TABLE_NAME = "_form_info_submission_association";
+
+	private static final DataField URI_MD5_SUBMISSION_FORM_ID = new DataField("URI_MD5_SUBMISSION_FORM_ID",
+			DataField.DataType.URI, false, PersistConsts.URI_STRING_LEN).setIndexable(IndexType.HASH);
+	
+	private static final DataField URI_MD5_FORM_ID = new DataField("URI_MD5_FORM_ID",
+			DataField.DataType.URI, false, PersistConsts.URI_STRING_LEN).setIndexable(IndexType.HASH);
+	
+	private static final DataField SUBMISSION_FORM_ID = new DataField("SUBMISSION_FORM_ID",
+			DataField.DataType.STRING, true, PersistConsts.MAX_SIMPLE_STRING_LEN);
+
+	private static final DataField SUBMISSION_MODEL_VERSION = new DataField("SUBMISSION_MODEL_VERSION",
+			DataField.DataType.INTEGER, true);
+
+	private static final DataField SUBMISSION_UI_VERSION = new DataField("SUBMISSION_UI_VERSION",
+			DataField.DataType.INTEGER, true);
+
+	private static final DataField IS_PERSISTENCE_MODEL_COMPLETE = new DataField("IS_PERSISTENCE_MODEL_COMPLETE",
+			DataField.DataType.BOOLEAN, true);
+
+	private static final DataField IS_SUBMISSION_ALLOWED = new DataField("IS_SUBMISSION_ALLOWED",
+			DataField.DataType.BOOLEAN, true);
+
+	private static final DataField URI_SUBMISSION_DATA_MODEL = new DataField("URI_SUBMISSION_DATA_MODEL",
+			DataField.DataType.URI, true);
+
+	public final DataField uriMd5SubmissionFormId;
+	public final DataField uriMd5FormId;
+	public final DataField submissionFormId;
+	public final DataField submissionModelVersion;
+	public final DataField submissionUiVersion;
+	public final DataField isPersistenceModelComplete;
+	public final DataField isSubmissionAllowed;
+	public final DataField uriSubmissionDataModel;
+
+	public static final String URI_FORM_ID_VALUE_FORM_INFO_SUBMISSION_ASSOCIATION = "aggregate.opendatakit.org:FormInfoSubmissionAssociation";
+
+	/**
+	 * DOM_AURI (md5 of submission form_id)
+	 * SUB_AURI (URI_FORM_INFO)
+	 */
+	/**
+	 * Construct a relation prototype.
+	 * 
+	 * @param databaseSchema
+	 */
+	private SubmissionAssociationTable(String databaseSchema) {
+		super(databaseSchema, TABLE_NAME);
+
+		fieldList.add(uriMd5SubmissionFormId = new DataField(URI_MD5_SUBMISSION_FORM_ID));
+		fieldList.add(uriMd5FormId = new DataField(URI_MD5_FORM_ID));
+		fieldList.add(submissionFormId = new DataField(SUBMISSION_FORM_ID));
+		fieldList.add(submissionModelVersion = new DataField(SUBMISSION_MODEL_VERSION));
+		fieldList.add(submissionUiVersion = new DataField(SUBMISSION_UI_VERSION));
+		fieldList.add(isPersistenceModelComplete = new DataField(IS_PERSISTENCE_MODEL_COMPLETE));
+		fieldList.add(isSubmissionAllowed = new DataField(IS_SUBMISSION_ALLOWED));
+		fieldList.add(uriSubmissionDataModel = new DataField(URI_SUBMISSION_DATA_MODEL));
+		
+		fieldValueMap.put(primaryKey, SubmissionAssociationTable.URI_FORM_ID_VALUE_FORM_INFO_SUBMISSION_ASSOCIATION);
+	}
+
+	/**
+	 * Construct an empty entity.
+	 * 
+	 * @param ref
+	 * @param user
+	 */
+	private SubmissionAssociationTable(SubmissionAssociationTable ref, User user) {
+		super(ref, user);
+		uriMd5SubmissionFormId = ref.uriMd5SubmissionFormId;
+		uriMd5FormId = ref.uriMd5FormId;
+		submissionFormId = ref.submissionFormId;
+		submissionModelVersion = ref.submissionModelVersion;
+		submissionUiVersion = ref.submissionUiVersion;
+		isPersistenceModelComplete = ref.isPersistenceModelComplete;
+		isSubmissionAllowed = ref.isSubmissionAllowed;
+		uriSubmissionDataModel = ref.uriSubmissionDataModel;
+	}
+
+	@Override
+	public SubmissionAssociationTable getEmptyRow(User user) {
+		return new SubmissionAssociationTable(this, user);
+	}
+	
+	public XFormParameters getXFormParameters() {
+		return new XFormParameters( getSubmissionFormId(), 
+				getSubmissionModelVersion(), getSubmissionUiVersion());
+	}
+	
+	public String getUriMd5FormId() {
+		return getStringField(uriMd5FormId);
+	}
+	
+	public void setUriMd5FormId(String value) {
+		if ( !setStringField(uriMd5FormId, value) ) {
+			throw new IllegalStateException("overflow uriMd5FormId");
+		}
+	}
+	
+	public String getUriMd5SubmissionFormId() {
+		return getStringField(uriMd5SubmissionFormId);
+	}
+	
+	public void setUriMd5SubmissionFormId(String value) {
+		if ( !setStringField(uriMd5SubmissionFormId, value) ) {
+			throw new IllegalStateException("overflow uriMd5SubmissionFormId");
+		}
+	}
+	
+	public String getSubmissionFormId() {
+		return getStringField(submissionFormId);
+	}
+	
+	public void setSubmissionFormId(String value) {
+		if ( !setStringField(submissionFormId, value) ) {
+			throw new IllegalStateException("overflow submissionFormId");
+		}
+	}
+
+	public Long getSubmissionModelVersion() {
+		return getLongField(submissionModelVersion);
+	}
+
+	public void setSubmissionModelVersion(Long value) {
+		setLongField(submissionModelVersion, value);
+	}
+
+	public Long getSubmissionUiVersion() {
+		return getLongField(submissionUiVersion);
+	}
+
+	public void setSubmissionUiVersion(Long value) {
+		setLongField(submissionUiVersion, value);
+	}
+
+	public Boolean getIsPersistenceModelComplete() {
+		return getBooleanField(isPersistenceModelComplete);
+	}
+
+	public void setIsPersistenceModelComplete(Boolean value) {
+		setBooleanField(isPersistenceModelComplete, value);
+	}
+
+	public Boolean getIsSubmissionAllowed() {
+		return getBooleanField(isSubmissionAllowed);
+	}
+
+	public void setIsSubmissionAllowed(Boolean value) {
+		setBooleanField(isSubmissionAllowed, value);
+	}
+
+	public String getUriSubmissionDataModel() {
+		return getStringField(uriSubmissionDataModel);
+	}
+
+	public void setUriSubmissionDataModel(String value) {
+		if ( !setStringField(uriSubmissionDataModel, value) ) {
+			throw new IllegalStateException("overflow uriSubmissionDataModel");
+		}
+	}
+
+	private static SubmissionAssociationTable relation = null;
+	
+	public static synchronized final SubmissionAssociationTable assertRelation(CallingContext cc) throws ODKDatastoreException {
+		if ( relation == null ) {
+			Datastore ds = cc.getDatastore();
+			User user = cc.getCurrentUser();
+			SubmissionAssociationTable relationPrototype;
+			relationPrototype = new SubmissionAssociationTable(ds.getDefaultSchemaName());
+			ds.assertRelation(relationPrototype, user); // may throw exception...
+		    // at this point, the prototype has become fully populated
+		    relation = relationPrototype; // set static variable only upon success...
+		}
+		return relation;
+	}
+}
---
+++ src/org/opendatakit/common/security/spring/RegisteredUsersTable.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2010 University of Washington
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.opendatakit.common.security.spring;
+
+import org.opendatakit.common.persistence.CommonFieldsBase;
+import org.opendatakit.common.persistence.DataField;
+import org.opendatakit.common.persistence.Datastore;
+import org.opendatakit.common.persistence.DataField.IndexType;
+import org.opendatakit.common.persistence.exception.ODKDatastoreException;
+import org.opendatakit.common.security.User;
+
+/**
+ * Table of registered users of the system.  Currently, only the 
+ * password fields, the SALT and the IS_CREDENTIALS_NON_EXPIRED 
+ * and IS_ENABLED fields are exposed to the user.
+ * 
+ * @author mitchellsundt@gmail.com
+ *
+ */
+public final class RegisteredUsersTable extends CommonFieldsBase {
+	private static final String TABLE_NAME = "_registered_users";
+	
+	private static final DataField BASIC_AUTH_PASSWORD = new DataField(
+			"BASIC_AUTH_PASSWORD", DataField.DataType.STRING, true );
+
+	private static final DataField BASIC_AUTH_SALT = new DataField(
+			"BASIC_AUTH_SALT", DataField.DataType.STRING, true, 8L );
+
+	private static final DataField DIGEST_AUTH_PASSWORD = new DataField(
+			"DIGEST_AUTH_PASSWORD", DataField.DataType.STRING, true );
+
+	private static final DataField IS_CREDENTIALS_NON_EXPIRED = new DataField(
+			"IS_CREDENTIALS_NON_EXPIRED", DataField.DataType.BOOLEAN, false );
+
+	private static final DataField IS_ENABLED = new DataField(
+			"IS_ENABLED", DataField.DataType.BOOLEAN, false );
+
+	private final DataField basicAuthPassword;
+	private final DataField basicAuthSalt;
+	private final DataField digestAuthPassword;
+	private final DataField isCredentialNonExpired; // relates to the above credentials
+	private final DataField isEnabled;
+	
+	protected RegisteredUsersTable(String schemaName) {
+		super(schemaName, TABLE_NAME);
+		fieldList.add(basicAuthPassword=new DataField(BASIC_AUTH_PASSWORD));
+		fieldList.add(basicAuthSalt=new DataField(BASIC_AUTH_SALT));
+		fieldList.add(digestAuthPassword=new DataField(DIGEST_AUTH_PASSWORD));
+		fieldList.add(isCredentialNonExpired=new DataField(IS_CREDENTIALS_NON_EXPIRED));
+		fieldList.add(isEnabled=new DataField(IS_ENABLED));
+		primaryKey.setIndexable(IndexType.ORDERED);
+	}
+	
+	protected RegisteredUsersTable(RegisteredUsersTable ref, User user) {
+		super(ref, user);
+		basicAuthPassword = ref.basicAuthPassword;
+		basicAuthSalt = ref.basicAuthSalt;
+		digestAuthPassword = ref.digestAuthPassword;
+		isCredentialNonExpired = ref.isCredentialNonExpired;
+		isEnabled = ref.isEnabled;
+	}
+
+	@Override
+	public CommonFieldsBase getEmptyRow(User user) {
+		return new RegisteredUsersTable(this, user);
+	}
+	
+	public String getUriUser() {
+		return getStringField(primaryKey);
+	}
+	
+	public void setUriUser(String value) {
+		if ( !setStringField(primaryKey, value)) {
+			throw new IllegalStateException("overflow primaryKey");
+		}
+	}
+	
+	public String getBasicAuthPassword() {
+		return getStringField(basicAuthPassword);
+	}
+
+	public void setBasicAuthPassword(String value) {
+		if ( !setStringField(basicAuthPassword, value)) {
+			throw new IllegalStateException("overflow basicAuthPassword");
+		}
+	}
+	
+	public String getBasicAuthSalt() {
+		return getStringField(basicAuthSalt);
+	}
+
+	public void setBasicAuthSalt(String value) {
+		if ( !setStringField(basicAuthSalt, value)) {
+			throw new IllegalStateException("overflow basicAuthSalt");
+		}
+	}
+
+	public String getDigestAuthPassword() {
+		return getStringField(digestAuthPassword);
+	}
+	
+	public void setDigestAuthPassword(String value) {
+		if ( !setStringField(digestAuthPassword, value)) {
+			throw new IllegalStateException("overflow digestAuthPassword");
+		}
+	}
+
+	public Boolean getIsCredentialNonExpired() {
+		return getBooleanField(isCredentialNonExpired);
+	}
+
+	public void setIsCredentialNonExpired(Boolean value) {
+		setBooleanField(isCredentialNonExpired, value);
+	}
+
+	public Boolean getIsEnabled() {
+		return getBooleanField(isEnabled);
+	}
+
+	public void setIsEnabled(Boolean value) {
+		setBooleanField(isEnabled, value);
+	}
+
+	private static RegisteredUsersTable relation = null;
+	
+	public static synchronized RegisteredUsersTable assertRelation(Datastore datastore, User user) throws ODKDatastoreException {
+		if ( relation == null ) {
+			RegisteredUsersTable relationPrototype;
+			relationPrototype = new RegisteredUsersTable(datastore.getDefaultSchemaName());
+			datastore.assertRelation(relationPrototype, user);
+			relation = relationPrototype;
+		}
+		return relation;
+	}
+
+}

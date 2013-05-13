218d730641716093300fb51285b98fefe0a6d456
mitchellsundt <mitchellsundt@gmail.com>
2013-05-13 12:42:17 -0700

    issue 824 - fix for json publishing of media attachments

---
+++ src/main/java/org/opendatakit/aggregate/client/popups/AbstractPopupBase.java
@@ -0,0 +1,29 @@
+package org.opendatakit.aggregate.client.popups;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.PopupPanel;
+
+public class AbstractPopupBase extends PopupPanel {
+
+  public AbstractPopupBase() {
+    super(false);
+    setModal(true); // things not in the popup are inactive
+
+    // Set glass behind the popup so that the things behind it are grayed out.
+    this.setGlassEnabled(true);
+    this.setGlassStyleName("gwt-PopupPanelGlassAggregate");
+
+  }
+
+  public PopupPanel.PositionCallback getPositionCallBack() {
+    return new PopupPanel.PositionCallback() {
+      @Override
+      public void setPosition(int offsetWidth, int offsetHeight) {
+          int left = Window.getScrollLeft() + ((Window.getClientWidth() - offsetWidth) / 2);
+          int top = Window.getScrollTop() + ((Window.getClientHeight() - offsetHeight) / 2);
+          setPopupPosition(left, top);
+      }
+    };
+  }
+
+}
